992aa42f2cb5724f6cb2ec891b857e0163cd4511
davkam <david.jr.kamran@gmail.com>
2023-06-16 17:41:19 +0200

    Minor code updates.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using ReservationManager.Exceptions;
-using ReservationManager.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -15,41 +13,9 @@ namespace ReservationManager
     /// </summary>
     public partial class App : Application
     {
-        //protected override void OnStartup(StartupEventArgs e)
-        //{
-        //    Reserver reserver = new Reserver("David Kamran");
-
-        //    try
-        //    {
-        //        reserver.MakeReservation(new Reservation(
-        //            new Reservable("Book", "Book for reading"),
-        //            "1647",
-        //            new DateTime(2023, 6, 15),
-        //            new DateTime(2023, 6, 16)
-        //            ));
-
-        //        reserver.MakeReservation(new Reservation(
-        //            new Reservable("Bike", "Bike for riding"),
-        //            "1647",
-        //            new DateTime(2023, 6, 17),
-        //            new DateTime(2023, 6, 19)
-        //            ));
-
-        //        reserver.MakeReservation(new Reservation(
-        //            new Reservable("Book", "Book for reading"),
-        //            "1647",
-        //            new DateTime(2023, 6, 15),
-        //            new DateTime(2023, 6, 16)
-        //            ));
-        //    }
-        //    catch (ReservationConflictException exception)
-        //    {
-        //        MessageBox.Show(exception.Message);
-        //    }
-
-        //    IEnumerable<Reservation> reservations = reserver.GetReservationsForReserver("1647");
-
-        //    base.OnStartup(e);
-        //}
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
     }
 }
